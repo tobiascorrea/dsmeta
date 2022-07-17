bee4498e268bae5976f15f4fd03efbfe281cd124
tobiascorrea <thobias2501@gmail.com>
2022-07-16 23:14:40 -0300

    useState

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,9 +1,13 @@
-import NotificationButton from '../notificationButton'
+import NotificationButton from '../notificationButton';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import './styles.css'
+import './styles.css';
+import { useState } from 'react';
 function SalesCard() {
-
+    const min = new Date(new Date().setDate(new Date().getDate() - 365));
+    const max = new Date();
+    const [minDate, setMinDate] = useState(min);
+    const [maxDate, setMaxDate] = useState(max);
     return (
 
         <div className="dsmeta-card">
@@ -11,16 +15,16 @@ function SalesCard() {
             <div>
                 <div className="dsmeta-form-control-container">
                     <DatePicker
-                        selected={new Date()}
-                        onChange={(date: Date) => { }}
+                        selected={maxDate}
+                        onChange={(date: Date) => setMaxDate(date)}
                         className="dsmeta-form-control"
                         dateFormat="dd/MM/yyyy"
                     />
                 </div>
                 <div className="dsmeta-form-control-container">
                     <DatePicker
-                        selected={new Date()}
-                        onChange={(date: Date) => { }}
+                        selected={minDate}
+                        onChange={(date: Date) => setMinDate(date)}
                         className="dsmeta-form-control"
                         dateFormat="dd/MM/yyyy"
                     />
@@ -44,7 +48,7 @@ function SalesCard() {
                         <tr>
                             <td className="show992">#341</td>
                             <td className="show576">08/07/2022</td>
-                            <td>Anakin</td>
+                            <td>Thobias</td>
                             <td className="show992">15</td>
                             <td className="show992">11</td>
                             <td>R$ 55300.00</td>
@@ -55,9 +59,9 @@ function SalesCard() {
                             </td>
                         </tr>
                         <tr>
-                            <td className="show992">#341</td>
+                            <td className="show992">#342</td>
                             <td className="show576">08/07/2022</td>
-                            <td>Anakin</td>
+                            <td>Priscila</td>
                             <td className="show992">15</td>
                             <td className="show992">11</td>
                             <td>R$ 55300.00</td>
@@ -68,9 +72,22 @@ function SalesCard() {
                             </td>
                         </tr>
                         <tr>
-                            <td className="show992">#341</td>
+                            <td className="show992">#343</td>
                             <td className="show576">08/07/2022</td>
-                            <td>Anakin</td>
+                            <td>Marcia</td>
+                            <td className="show992">15</td>
+                            <td className="show992">11</td>
+                            <td>R$ 55300.00</td>
+                            <td>
+                                <div className="dsmeta-red-btn-container">
+                                    <NotificationButton />
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="show992">#344</td>
+                            <td className="show576">08/07/2022</td>
+                            <td>Paulo</td>
                             <td className="show992">15</td>
                             <td className="show992">11</td>
                             <td>R$ 55300.00</td>
@@ -90,4 +107,4 @@ function SalesCard() {
     )
 }
 
-export default SalesCard
+export default SalesCard;
